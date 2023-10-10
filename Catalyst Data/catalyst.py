fcfa518d564d2bfe240e18d944cc133156ea1c7d
Shannon <98058003+ShannonatODU@users.noreply.github.com>
2023-10-10 10:06:27 -0400

    Add files via upload

--- Catalyst Data/catalyst.py
+++ Catalyst Data/catalyst.py
@@ -1,20 +1,19 @@
-from morfeus import ConeAngle, read_geometry, SASA, BuriedVolume
+import numpy as np
+from morfeus import read_geometry, SASA, BuriedVolume
 from morfeus import Pyramidalization, Sterimol
+import matplotlib.pyplot as plt
 
 #Ir2mphen catalyst
 elements, coordinates = read_geometry("Ir2mphen.gjf")
 
-cone_angle = ConeAngle(elements, coordinates, 61)
-print("Ir2mphen cone angle", cone_angle.cone_angle)
-
-bv = BuriedVolume(elements, coordinates, 61, excluded_atoms=[1,3,4,5,16,6,8,10,11,15,13,12,83,11,
-                                                             54,60,59,37,35,41,36,49,45,
-                                                             53,58,57,31,18,17,19,23,27,
-                                                             62,64,63,65,67,68,66,69,70])
+bv = BuriedVolume(elements, coordinates, 59, excluded_atoms=[51,55,56,15,16,17,21,25,29,
+                                                             52,57,58,33,34,35,39,43,47,
+                                                             60,61,62,63,64,65,66,67,68],
+						             z_axis_atoms=[53,54])
 bv.print_report()
+bv.plot_steric_map(filename='Ir2mphen')
 
-
-pyr = Pyramidalization(coordinates, 61)
+pyr = Pyramidalization(coordinates, 59)
 print("Ir2mphen Pyramidalization By Agranat and Radhakrishnan", pyr.P)
 print("Ir2mphen Pyramidalization By Gavrish", pyr.P_angle)
 
@@ -22,23 +21,25 @@ sasa = SASA(elements, coordinates)
 print("Ir2mphen SASA area", sasa.area)
 print("Ir2mphen SASA voleume", sasa.volume)
 
-sterimol = Sterimol(elements, coordinates, 61, 83)
+sterimol = Sterimol(elements, coordinates, 59, 81)
 print("Ir2mphen Sterimol L" , sterimol.L_value)
 print("Ir2mphen Sterimol B1", sterimol.B_1_value)
 print("Ir2mphen Sterimol B5", sterimol.B_5_value)
 
+sterimol.bury(method="delete")
+print("Buried Ir2mphen Sterimol L" , sterimol.L_value)
+print("Buried Ir2mphen Sterimol B1", sterimol.B_1_value)
+print("Buried Ir2mphen Sterimol B5", sterimol.B_5_value)
+
 #Ir4mphen catalyst
 elements, coordinates = read_geometry("Ir4mphen.gjf")
 
-cone_angle = ConeAngle(elements, coordinates, 61)
-print("Ir4mphen cone angle", cone_angle.cone_angle)
-
-bv = BuriedVolume(elements, coordinates, 61, excluded_atoms=[1,3,4,5,16,6,8,10,11,15,13,12,83,87,95,91,
-                                                             54,60,59,37,35,41,36,49,45,
+bv = BuriedVolume(elements, coordinates, 61, excluded_atoms=[54,60,59,37,35,41,36,49,45,
                                                              53,58,57,31,18,17,19,23,27,
-                                                             62,64,63,65,67,68,66,69,70])
+                                                             62,64,63,65,67,68,66,69,70],
+						             z_axis_atoms=[56,55])
 bv.print_report()
-#print("buried volume", bv)
+bv.plot_steric_map(filename='Ir4mphen')
 
 pyr = Pyramidalization(coordinates, 61)
 print("Ir4mphen Pyramidalization By Agranat and Radhakrishnan", pyr.P)
@@ -48,25 +49,25 @@ sasa = SASA(elements, coordinates)
 print("Ir4mphen SASA area", sasa.area)
 print("Ir4mphen SASA voleume", sasa.volume)
 
-sterimol = Sterimol(elements, coordinates, 61, 83)
+sterimol = Sterimol(elements, coordinates, 61, 96)
 print("Ir4mphen Sterimol L" , sterimol.L_value)
 print("Ir4mphen Sterimol B1", sterimol.B_1_value)
 print("Ir4mphen Sterimol B5", sterimol.B_5_value)
 
-
+sterimol.bury(method="delete")
+print("Buried Ir4mphen Sterimol L" , sterimol.L_value)
+print("Buried Ir4mphen Sterimol B1", sterimol.B_1_value)
+print("Buried Ir4mphen Sterimol B5", sterimol.B_5_value)
 
 #Irdtpby catalyst
 elements, coordinates = read_geometry("Irdtpby.gjf")
 
-cone_angle = ConeAngle(elements, coordinates, 57)
-print("Irdtpby cone angle", cone_angle.cone_angle)
-
-bv = BuriedVolume(elements, coordinates, 57, excluded_atoms=[1,3,4,5,12,11,6,7,8,9,83,97,105,101,84,85,93,89,
-                                                             58,59,60,61,62,63,64,65,66,
+bv = BuriedVolume(elements, coordinates, 57, excluded_atoms=[58,59,60,61,62,63,64,65,66,
                                                              49,53,54,13,15,19,14,27,23,
-                                                             50,55,56,31,33,37,32,54,41])
+                                                             50,55,56,31,33,37,32,45,41],
+						             z_axis_atoms=[51,52])
 bv.print_report()
-#print("buried volume", bv)
+bv.plot_steric_map(filename='Irdtpby')
 
 pyr = Pyramidalization(coordinates, 57)
 print("Irdtpby Pyramidalization By Agranat and Radhakrishnan", pyr.P)
@@ -76,25 +77,25 @@ sasa = SASA(elements, coordinates)
 print("Irdtpby SASA area", sasa.area)
 print("Irdtpby SASA voleume", sasa.volume)
 
-sterimol = Sterimol(elements, coordinates, 57, 93)
+sterimol = Sterimol(elements, coordinates, 57, 108)
 print("Irdtpby Sterimol L" , sterimol.L_value)
 print("Irdtpby Sterimol B1", sterimol.B_1_value)
 print("Irdtpby Sterimol B5", sterimol.B_5_value)
 
-
+sterimol.bury(method="delete")
+print("Buried Irdtpby Sterimol L" , sterimol.L_value)
+print("Buried Irdtpby Sterimol B1", sterimol.B_1_value)
+print("Buried Irdtpby Sterimol B5", sterimol.B_5_value)
 
 #Irdmpe catalyst
 elements, coordinates = read_geometry("Irdmpe.gjf")
 
-cone_angle = ConeAngle(elements, coordinates, 72)
-print("Irdmpe cone angle", cone_angle.cone_angle)
-
-bv = BuriedVolume(elements, coordinates, 72, excluded_atoms=[85,81,65,69,73,77,
-                                                             38,42,41,20,29,33,19,25,21,
+bv = BuriedVolume(elements, coordinates, 72, excluded_atoms=[38,42,41,20,29,33,19,25,21,
                                                              37,39,40,1,2,3,7,11,15,
-                                                             43,44,45,48,49,56,47,50,51])
+                                                             43,44,45,48,49,56,47,50,51],
+						             z_axis_atoms=[64,68])
 bv.print_report()
-#print("buried volume", bv)
+bv.plot_steric_map(filename='Irdmpe')
 
 pyr = Pyramidalization(coordinates, 72)
 print("Irdmpe Pyramidalization By Agranat and Radhakrishnan", pyr.P)
@@ -104,25 +105,25 @@ sasa = SASA(elements, coordinates)
 print("Irdmpe SASA area", sasa.area)
 print("Irdmpe SASA voleume", sasa.volume)
 
-sterimol = Sterimol(elements, coordinates, 72, 85)
+sterimol = Sterimol(elements, coordinates, 72, 88)
 print("Irdmpe Sterimol L" , sterimol.L_value)
 print("Irdmpe Sterimol B1", sterimol.B_1_value)
 print("Irdmpe Sterimol B5", sterimol.B_5_value)
 
+sterimol.bury(method="delete")
+print("Buried Irdmpe Sterimol L" , sterimol.L_value)
+print("Buried Irdmpe Sterimol B1", sterimol.B_1_value)
+print("BUried Irdmpe Sterimol B5", sterimol.B_5_value)
+
 #Irdppe catalyst
 elements, coordinates = read_geometry("Irdppe.gjf")
 
-cone_angle = ConeAngle(elements, coordinates, 116)
-print("Irdppe cone angle", cone_angle.cone_angle)
-
-bv = BuriedVolume(elements, coordinates, 116, excluded_atoms=[65,91,66,67,68,69,70,71,
-                                                              72,73,74,75,76,77,
-                                                              92,93,94,95,96,97,
-                                                              38,41,42,20,19,33,29,25,21,
+bv = BuriedVolume(elements, coordinates, 116, excluded_atoms=[38,41,42,20,19,33,29,25,21,
                                                               37,39,40,1,3,7,2,11,15,
-                                                              43,44,45,47,50,51,46,48,49])
+                                                              43,44,45,47,50,51,46,48,49],
+						             z_axis_atoms=[64,90])
 bv.print_report()
-#print("buried volume", bv)
+bv.plot_steric_map(filename='Irdppe')
 
 pyr = Pyramidalization(coordinates, 116)
 print("Irdppe Pyramidalization By Agranat and Radhakrishnan", pyr.P)
@@ -132,52 +133,56 @@ sasa = SASA(elements, coordinates)
 print("Irdppe SASA area", sasa.area)
 print("Irdppe SASA voleume", sasa.volume)
 
-sterimol = Sterimol(elements, coordinates, 116, 75)
+sterimol = Sterimol(elements, coordinates, 116, 108)
 print("Irdppe Sterimol L" , sterimol.L_value)
 print("Irdppe Sterimol B1", sterimol.B_1_value)
 print("Irdppe Sterimol B5", sterimol.B_5_value)
 
-#IrmphenPh catalyst
-elements, coordinates = read_geometry("IrmphenPh.gjf")
+sterimol.bury(method="delete")
+print("Buried Irdppe Sterimol L" , sterimol.L_value)
+print("Buried Irdppe Sterimol B1", sterimol.B_1_value)
+print("Buried Irdppe Sterimol B5", sterimol.B_5_value)
 
-cone_angle = ConeAngle(elements, coordinates, 61)
-print("IrmphenPh cone angle", cone_angle.cone_angle)
 
-bv = BuriedVolume(elements, coordinates, 61, excluded_atoms=[13,12,11,10,15,8,6,5,4,3,1,87,86,88,89,91,93,
-                                                                  54,60,59,36,49,45,41,35,37,
-                                                                  53,57,58,17,19,23,18,27,31,
-                                                                  62,63,64,65,66,67,68,69,70])
+
+#Ir5Phbpy catalyst
+elements, coordinates = read_geometry("Ir5Phbpy.gjf")
+
+bv = BuriedVolume(elements, coordinates, 57, excluded_atoms=[49,53,54,13,14,15,19,27,23,
+                                                                  50,55,56,31,33,37,41,32,45,
+                                                                  58,59,60,61,62,66,65,64,63],
+						             z_axis_atoms=[51,52])
 bv.print_report()
-#print("buried volume", bv)
+bv.plot_steric_map(filename='Ir5Phbpy')
 
-pyr = Pyramidalization(coordinates, 61)
-print("IrmphenPh Pyramidalization By Agranat and Radhakrishnan", pyr.P)
-print("IrmphenPh Pyramidalization By Gavrish", pyr.P_angle)
+pyr = Pyramidalization(coordinates, 57)
+print("Ir5Phbpy Pyramidalization By Agranat and Radhakrishnan", pyr.P)
+print("Ir5Phbpy Pyramidalization By Gavrish", pyr.P_angle)
 
 sasa = SASA(elements, coordinates)
-print("IrmphenPh SASA area", sasa.area)
-print("IrmphenPh SASA voleume", sasa.volume)
+print("Ir5Phbpy SASA area", sasa.area)
+print("Ir5Phbpy SASA voleume", sasa.volume)
 
-sterimol = Sterimol(elements, coordinates, 61, 93)
-print("IrmphenPh Sterimol L" , sterimol.L_value)
-print("IrmphenPh Sterimol B1", sterimol.B_1_value)
-print("IrmphenPh Sterimol B5", sterimol.B_5_value)
+sterimol = Sterimol(elements, coordinates, 57, 92)
+print("Ir5Phbpy Sterimol L" , sterimol.L_value)
+print("Ir5Phbpy Sterimol B1", sterimol.B_1_value)
+print("Ir5Phbpy Sterimol B5", sterimol.B_5_value)
 
-
+sterimol.bury(method="delete")
+print("Buried Ir5Phbpy Sterimol L" , sterimol.L_value)
+print("Buried Ir5Phbpy Sterimol B1", sterimol.B_1_value)
+print("Buried Ir5Phbpy Sterimol B5", sterimol.B_5_value)
 
 
 #IrSMAP catalyst
 elements, coordinates = read_geometry("IrSMAP.gjf")
 
-cone_angle = ConeAngle(elements, coordinates, 43)
-print("IrSMAP cone angle", cone_angle.cone_angle)
-
-bv = BuriedVolume(elements, coordinates, 43,excluded_atoms=[69,70,71,72,67,68,66,85,86,87,88,89,90,98,94,
-                                                            44,45,46,47,48,49,50,51,52,
+bv = BuriedVolume(elements, coordinates, 43,excluded_atoms=[44,45,46,47,48,49,50,51,52,
                                                             38,41,42,20,29,33,19,21,25,
-                                                            37,39,40,1,2,3,7,11,15])
+                                                            37,39,40,1,2,3,7,11,15],
+						             z_axis_atoms=[65,66])
 bv.print_report()
-#print("buried volume", bv)
+bv.plot_steric_map(filename='IrSMAP')
 
 pyr = Pyramidalization(coordinates, 43)
 print("IrSMAP Pyramidalization By Agranat and Radhakrishnan", pyr.P)
@@ -187,24 +192,25 @@ sasa = SASA(elements, coordinates)
 print("IrSMAP SASA area", sasa.area)
 print("IrSMAP SASA voleume", sasa.volume)
 
-sterimol = Sterimol(elements, coordinates, 43, 94)
+sterimol = Sterimol(elements, coordinates, 43, 95)
 print("IrSMAP Sterimol L" , sterimol.L_value)
 print("IrSMAP Sterimol B1", sterimol.B_1_value)
 print("IrSMAP Sterimol B5", sterimol.B_5_value)
 
+sterimol.bury(method="delete")
+print("Buried IrSMAP Sterimol L" , sterimol.L_value)
+print("Buried IrSMAP Sterimol B1", sterimol.B_1_value)
+print("Buried IrSMAP Sterimol B5", sterimol.B_5_value)
 
 #RuCpstar catalyst
 elements, coordinates = read_geometry("RuCpstar.gjf")
 
-cone_angle = ConeAngle(elements, coordinates, 89)
-print("RuCpstar cone angle", cone_angle.cone_angle)
-
-bv = BuriedVolume(elements, coordinates, 89, excluded_atoms=[10,11,13,14,15,19,23,27,31,
-                                                             7,1,2,35,36,37,41,49,45,
+bv = BuriedVolume(elements, coordinates, 89, excluded_atoms=[7,1,2,35,36,37,41,49,45,
                                                              8,3,4,53,55,59,54,63,67,
-                                                             9,5,6,71,73,77,72,81,85])
+                                                             9,5,6,71,73,77,72,81,85],
+						             z_axis_atoms=[10,11,12])
 bv.print_report()
-#print("buried volume", bv)
+bv.plot_steric_map(filename='RuCpstar')
 
 pyr = Pyramidalization(coordinates, 89)
 print("RuCpstar Pyramidalization By Agranat and Radhakrishnan", pyr.P)
@@ -214,24 +220,26 @@ sasa = SASA(elements, coordinates)
 print("RuCpstar SASA area", sasa.area)
 print("RuCpstar SASA voleume", sasa.volume)
 
-sterimol = Sterimol(elements, coordinates,89, 15)
+sterimol = Sterimol(elements, coordinates,89, 24)
 print("RuCpstar Sterimol L" , sterimol.L_value)
 print("RuCpstar Sterimol B1", sterimol.B_1_value)
 print("RuCpstar Sterimol B5", sterimol.B_5_value)
 
+sterimol.bury(method="delete")
+print("Buried RuCPstar Sterimol L" , sterimol.L_value)
+print("Buried RuCPstar Sterimol B1", sterimol.B_1_value)
+print("Buried RuCPstar Sterimol B5", sterimol.B_5_value)
+
 #RhCpstar catalyst
 elements, coordinates = read_geometry("RhCpstar.gjf")
 
-cone_angle = ConeAngle(elements, coordinates, 1)
-print("RhCpstar cone angle", cone_angle.cone_angle)
-
-bv = BuriedVolume(elements, coordinates, 1, excluded_atoms=[2,4,5,6,7,11,15,19,23,
-                                                            79,83,70,69,71,75,87,88,89,
+bv = BuriedVolume(elements, coordinates, 1, excluded_atoms=[79,83,70,69,71,75,87,88,89,
                                                             90,91,92,94,98,97,93,96,95,
                                                             48,49,50,51,53,54,52,56,55,
-                                                            27,28,29,30,31,34,35,32,33])
+                                                            27,28,29,30,31,34,35,32,33],
+						             z_axis_atoms=[2,3,6])
 bv.print_report()
-#print("buried volume", bv)
+bv.plot_steric_map(filename='RhCpstar')
 
 pyr = Pyramidalization(coordinates, 1)
 print("RhCpstar Pyramidalization By Agranat and Radhakrishnan", pyr.P)
@@ -241,12 +249,44 @@ sasa = SASA(elements, coordinates)
 print("RhCpstar SASA area", sasa.area)
 print("RhCpstar SASA voleume", sasa.volume)
 
-sterimol = Sterimol(elements, coordinates, 1, 15)
+sterimol = Sterimol(elements, coordinates, 1, 25)
 print("RhCpstar Sterimol L" , sterimol.L_value)
 print("RhCpstar Sterimol B1", sterimol.B_1_value)
 print("RhCpstar Sterimol B5", sterimol.B_5_value)
 
+sterimol.bury(method="delete")
+print("Buried RhCpstar Sterimol L" , sterimol.L_value)
+print("Buried RhCpstar Sterimol B1", sterimol.B_1_value)
+print("Buried RhCpstar Sterimol B5", sterimol.B_5_value)
 
+# catalyst
+elements, coordinates = read_geometry("IrtAs.gjf")
+
+bv = BuriedVolume(elements, coordinates, 108, excluded_atoms=[ 43, 44, 45, 46, 47,48, 49, 50, 51, 
+							      37, 39, 40, 1,
+                                                              2, 3, 7, 11, 15, 38, 42, 41, 20,
+                                                              33, 29, 19, 25, 21],
+						             z_axis_atoms=[131,132])
+bv.print_report()
+bv.plot_steric_map(filename='IrtAs')
+
+pyr = Pyramidalization(coordinates, 108)
+print("IrtAs Pyramidalization By Agranat and Radhakrishnan", pyr.P)
+print("IrtAs Pyramidalization By Gavrish", pyr.P_angle)
+
+sasa = SASA(elements, coordinates)
+print("IrtAs SASA area", sasa.area)
+print("IrtAs SASA voleume", sasa.volume)
+
+sterimol = Sterimol(elements, coordinates, 108, 130)
+print("IrtAs Sterimol L" , sterimol.L_value)
+print("IrtAs Sterimol B1", sterimol.B_1_value)
+print("IrtAs Sterimol B5", sterimol.B_5_value)
+
+sterimol.bury(method="delete")
+print("Buried IrtAs Sterimol L" , sterimol.L_value)
+print("Buried IrtAs Sterimol B1", sterimol.B_1_value)
+print("Buried IrtAs Sterimol B5", sterimol.B_5_value)
 
 
 
